00de05dd027432381989d87ffc0d53403696a560
nicro23 <modyhamody2332@gmail.com>
2025-04-20 18:42:20 +0200

    added working GUI

--- OS_proj/Form1.Designer.cs
+++ OS_proj/Form1.Designer.cs
@@ -47,22 +47,22 @@
             this.label8 = new System.Windows.Forms.Label();
             this.textBox6 = new System.Windows.Forms.TextBox();
             this.label9 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(479, 72);
+            this.button1.Location = new System.Drawing.Point(498, 164);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
             this.button1.Text = "add";
             this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(99, 40);
+            this.label1.Location = new System.Drawing.Point(99, 19);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(53, 13);
             this.label1.TabIndex = 1;
@@ -71,7 +71,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(29, 40);
+            this.label2.Location = new System.Drawing.Point(30, 20);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(45, 13);
             this.label2.TabIndex = 2;
@@ -80,7 +80,7 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(168, 40);
+            this.label3.Location = new System.Drawing.Point(168, 19);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(58, 13);
             this.label3.TabIndex = 3;
@@ -89,7 +89,7 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(291, 42);
+            this.label4.Location = new System.Drawing.Point(291, 21);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(65, 13);
             this.label4.TabIndex = 4;
@@ -98,7 +98,7 @@
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(381, 42);
+            this.label5.Location = new System.Drawing.Point(381, 21);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(84, 13);
             this.label5.TabIndex = 5;
@@ -107,7 +107,7 @@
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(41, 78);
+            this.label6.Location = new System.Drawing.Point(41, 54);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(23, 13);
             this.label6.TabIndex = 6;
@@ -115,28 +115,28 @@
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(102, 71);
+            this.textBox1.Location = new System.Drawing.Point(102, 50);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(50, 20);
             this.textBox1.TabIndex = 7;
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(171, 72);
+            this.textBox2.Location = new System.Drawing.Point(171, 51);
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(50, 20);
             this.textBox2.TabIndex = 8;
             // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(294, 72);
+            this.textBox3.Location = new System.Drawing.Point(294, 51);
             this.textBox3.Name = "textBox3";
             this.textBox3.Size = new System.Drawing.Size(62, 20);
             this.textBox3.TabIndex = 9;
             // 
             // textBox4
             // 
-            this.textBox4.Location = new System.Drawing.Point(384, 72);
+            this.textBox4.Location = new System.Drawing.Point(384, 51);
             this.textBox4.Name = "textBox4";
             this.textBox4.Size = new System.Drawing.Size(81, 20);
             this.textBox4.TabIndex = 10;
@@ -144,7 +144,7 @@
             // radioButton1
             // 
             this.radioButton1.AutoSize = true;
-            this.radioButton1.Location = new System.Drawing.Point(583, 100);
+            this.radioButton1.Location = new System.Drawing.Point(498, 78);
             this.radioButton1.Name = "radioButton1";
             this.radioButton1.Size = new System.Drawing.Size(78, 17);
             this.radioButton1.TabIndex = 11;
@@ -154,8 +154,9 @@
             // 
             // comboBox1
             // 
+            this.comboBox1.FlatStyle = System.Windows.Forms.FlatStyle.System;
             this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(583, 73);
+            this.comboBox1.Location = new System.Drawing.Point(498, 51);
             this.comboBox1.Name = "comboBox1";
             this.comboBox1.Size = new System.Drawing.Size(121, 21);
             this.comboBox1.TabIndex = 12;
@@ -163,7 +164,7 @@
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(582, 57);
+            this.label7.Location = new System.Drawing.Point(495, 20);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(50, 13);
             this.label7.TabIndex = 13;
@@ -172,7 +173,7 @@
             // radioButton2
             // 
             this.radioButton2.AutoSize = true;
-            this.radioButton2.Location = new System.Drawing.Point(583, 123);
+            this.radioButton2.Location = new System.Drawing.Point(498, 101);
             this.radioButton2.Name = "radioButton2";
             this.radioButton2.Size = new System.Drawing.Size(100, 17);
             this.radioButton2.TabIndex = 14;
@@ -182,7 +183,7 @@
             // 
             // textBox5
             // 
-            this.textBox5.Location = new System.Drawing.Point(235, 72);
+            this.textBox5.Location = new System.Drawing.Point(235, 51);
             this.textBox5.Name = "textBox5";
             this.textBox5.Size = new System.Drawing.Size(50, 20);
             this.textBox5.TabIndex = 16;
@@ -190,7 +191,7 @@
             // label8
             // 
             this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(232, 42);
+            this.label8.Location = new System.Drawing.Point(232, 21);
             this.label8.Name = "label8";
             this.label8.Size = new System.Drawing.Size(58, 13);
             this.label8.TabIndex = 15;
@@ -198,7 +199,7 @@
             // 
             // textBox6
             // 
-            this.textBox6.Location = new System.Drawing.Point(583, 160);
+            this.textBox6.Location = new System.Drawing.Point(498, 138);
             this.textBox6.Name = "textBox6";
             this.textBox6.Size = new System.Drawing.Size(81, 20);
             this.textBox6.TabIndex = 17;
@@ -206,17 +207,28 @@
             // label9
             // 
             this.label9.AutoSize = true;
-            this.label9.Location = new System.Drawing.Point(583, 144);
+            this.label9.Location = new System.Drawing.Point(498, 122);
             this.label9.Name = "label9";
             this.label9.Size = new System.Drawing.Size(50, 13);
             this.label9.TabIndex = 18;
             this.label9.Text = "Quantum";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(498, 79);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(80, 17);
+            this.checkBox1.TabIndex = 19;
+            this.checkBox1.Text = "checkBox1";
+            this.checkBox1.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.checkBox1);
             this.Controls.Add(this.label9);
             this.Controls.Add(this.textBox6);
             this.Controls.Add(this.textBox5);
@@ -265,6 +277,7 @@
         private System.Windows.Forms.Label label8;
         private System.Windows.Forms.TextBox textBox6;
         private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.CheckBox checkBox1;
     }
 }
 
--- OS_proj/Form1.cs
+++ OS_proj/Form1.cs
@@ -7,64 +7,312 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+using System.Xml.Linq;
 namespace OS_proj
 {
+    /* to add:
+     * 1. functional calc button
+     * 2. change prio to check box instead of being in combo box
+     * 3. make a best algo button that calcualtes the best algorithm
+     * 4. draw a grant chart when the user presses calculate
+     */
     public partial class Form1: Form
     {
+        Label P_label, BT, AT, Prio, WT, TA, Algo, quantum;
+        TextBox quantum_box;
+        ComboBox Algo_box;
+        Button b, calc;
+        RadioButton pre, non_pre;
         List<process> pl;
         process p;
+        int process_i;
         public Form1()
         {
-            InitializeComponent();
+            this.Paint += Form1_Paint;
+            this.Load += Form1_Load;
+            pl = new List<process>();
+            pre = new RadioButton();
+            non_pre = new RadioButton();
+            Algo_box = new ComboBox();
+            process_i = 1;
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void Algo_box_SelectedIndexChanged(object sender, EventArgs e)
         {
-            p = new process();
-            p.x = 100;
-            p.y = 100;
-            p.show(7);
-            /*button1.Text = "clicked";
-            Label l;
-            l = new Label();
-            l.Text = "sajdlkas";
-            l.Show();
-            l.Width = 100;
-            l.Height = 20;
-            l.Location = new Point(100, 100);*/
+            switch (Algo_box.Items[Algo_box.SelectedIndex].ToString())
+            {
+                case "First Come First Served":
+                    non_pre.Enabled = false;
+                    non_pre.Checked = true;
+                    pre.Enabled = false;
+                    pre.Checked = false;
+
+                    if (pl[0].Prio.ReadOnly == false)
+                    {
+                        for (int i = 0; i < pl.Count; i++)
+                        {
+                            pl[i].Prio.ReadOnly = true;
+                        }
+                    }
+                    quantum_box.ReadOnly = true;
+                    break;
+                case "Shortest Job First":
+
+                    if (pl[0].Prio.ReadOnly == false)
+                    {
+                        for (int i = 0; i < pl.Count; i++)
+                        {
+                            pl[i].Prio.ReadOnly = true;
+                        }
+                    }
+                    quantum_box.ReadOnly = true;
+                    non_pre.Enabled = true;
+                    pre.Enabled = true;
+                    break;
+                case "Round Robin":
+                    quantum_box.ReadOnly = false;
+                    non_pre.Enabled = false;
+                    non_pre.Checked = false;
+                    pre.Enabled = false;
+                    pre.Checked = true;
+
+                    if (pl[0].Prio.ReadOnly == false)
+                    {
+                        for (int i = 0; i < pl.Count; i++)
+                        {
+                            pl[i].Prio.ReadOnly = true;
+                        }
+                    }
+                    break;
+                case "Priority":
+                    for (int i = 0; i < pl.Count; i++)
+                    {
+                        pl[i].Prio.ReadOnly = false;
+                    }
+
+                    quantum_box.ReadOnly = true; 
+                    non_pre.Enabled = true;
+                    pre.Enabled = true;
+                    break;
+
+            }
         }
 
+        private void create_headers(int x_start,int y, int margin,int section_margin)
+        {
+            Label P_label = new Label();
+            P_label.Text = "Process";
+            P_label.AutoSize = true;
+            P_label.Location = new Point(x_start, y);
+            Controls.Add(P_label);
+            x_start += P_label.Width;
+            BT = new Label();
+            BT.Text = "Burst time";
+            BT.AutoSize = true;
+            BT.Location = new Point(x_start + margin, y);
+            Controls.Add(BT);
+            x_start += BT.Width + margin; 
+            AT = new Label();
+            AT.Text = "Arrival time";
+            AT.AutoSize = true;
+            AT.Location = new Point(x_start + margin, y);
+            Controls.Add(AT);
+            x_start += AT.Width + margin;
+            Prio = new Label();
+            Prio.Text = "Priority";
+            Prio.AutoSize = true;
+            Prio.Location = new Point(x_start + margin, y);
+            Controls.Add(Prio);
+            x_start += Prio.Width + margin + section_margin;
+            WT = new Label();
+            WT.Text = "Waiting time";
+            WT.AutoSize = true;
+            WT.Location = new Point(x_start + margin, y);
+            Controls.Add(WT);
+            x_start += WT.Width + margin;
+            TA = new Label();
+            TA.Text = "Turnaround time";
+            TA.AutoSize = true;
+            TA.Location = new Point(x_start + margin, y);
+            Controls.Add(TA);
+            x_start += TA.Width + margin;
+        }
         private void Form1_Load(object sender, EventArgs e)
         {
-            comboBox1.Items.Add("FCFS");
-            comboBox1.Items.Add("SJF");
-            comboBox1.Items.Add("RR");
-            comboBox1.Items.Add("Priority");
+            int margin, section_margin;
+            margin = 10;
+            section_margin = 20;
+            Size = new Size(815, 490);
+
+            create_headers(30, 20, margin, section_margin);
+
+
+            //add choices
+            Algo = new Label();
+            Algo.Text = "Algorithm: ";
+            Algo.AutoSize = true;
+            Algo.Location = new Point(465, 20);
+            Controls.Add(Algo);
+
+            Algo_box = new ComboBox();
+            Algo_box.Items.Add("First Come First Served");
+            Algo_box.Items.Add("Shortest Job First");
+            Algo_box.Items.Add("Round Robin");
+            Algo_box.Items.Add("Priority");
+            Algo_box.Size = new Size(130, 20);
+            Algo_box.Location = new Point(465, 45);
+            Controls.Add(Algo_box);
+
+            Algo_box.SelectedIndexChanged += Algo_box_SelectedIndexChanged;
+
+
+            //add radio_buttons
+            pre.Text = "Preemptive";
+            pre.Location = new Point(465, 75);
+            Controls.Add(pre);
+            non_pre.Text = "Non-Preemptive";
+            non_pre.Location = new Point(465, 95);
+            Controls.Add(non_pre);
+
+            //quantum input 
+            quantum = new Label();
+            quantum.Text = "Quantum: ";
+            quantum.AutoSize = true;
+            quantum.Location = new Point(465, 120);
+            Controls.Add(quantum);
+
+            quantum_box = new TextBox();
+            quantum_box.ReadOnly = true;
+            quantum_box.Size = new Size(quantum.Width, quantum.Height);
+            quantum_box.Location = new Point(465, 140);
+            Controls.Add(quantum_box);
+
+            //create buttons
+            b = new Button();
+            b.Text = "New process";
+            b.Location = new Point(465, 170);
+            b.Size = new Size(90, 25);
+            //b.AutoSize = true;
+            b.Click += B_Click;
+            Controls.Add(b);
+
+            calc = new Button();
+            calc.Text = "Calculate";
+            calc.Location = new Point(465, 200);
+            calc.AutoSize = true;
+            calc.Click += Calc_Click;
+            Controls.Add(calc);
+            //add first process
+            add_process(process_i, 35, 45, margin, section_margin);
+            process_i++;
+        }
+
+        private void Calc_Click(object sender, EventArgs e)
+        {
+            switch (Algo_box.Items[Algo_box.SelectedIndex].ToString())
+            {
+                case "First Come First Served":
+                //always non-preemptive
+                    break;
+
+                case "Shortest Job First":
+
+                    break;
+
+                case "Round Robin":
+                    //always preemptive
+                    break;
+
+                //will be changed to be a checkbox
+                case "Priority":
+                    break;
+            }
+        }
+
+        private void B_Click(object sender, EventArgs e)
+        {
+            int y_margin = 15 + (process_i * 30);
+            if (process_i <= 13)
+            {
+                add_process(process_i, 35, y_margin, 10, 20);
+                process_i++;
+            }
+        }
+
+        private void add_process(int i, int x_tmp, int y_tmp, int margin, int section_margin)
+        {
+            p = new process();
+            p.x = x_tmp;
+            p.y = y_tmp;
+
+            //name header
+            p.name.Text = "P" + i.ToString() + ":";
+            p.name.Font = new Font(p.name.Font.FontFamily, 10);
+            p.name.AutoSize = true;
+            //set locations
+            p.name.Location = new Point(p.x, p.y);
+            x_tmp += p.name.Width / 2;
+            p.BT.Location = new Point(x_tmp, p.y);
+            x_tmp += BT.Width + margin;
+            p.AT.Location = new Point(x_tmp, p.y);
+            x_tmp += AT.Width + margin;
+            p.Prio.Location = new Point(x_tmp, p.y);
+            x_tmp += Prio.Width + margin + section_margin;
+            p.WT.Location = new Point(x_tmp, p.y);
+            x_tmp += WT.Width + margin;
+            p.TA.Location = new Point(x_tmp, p.y);
+
+            //set sizes
+            p.BT.Size = new Size(BT.Width, BT.Height);
+            p.AT.Size = new Size(AT.Width, AT.Height);
+            p.Prio.Size = new Size(Prio.Width, Prio.Height);
+            p.WT.Size = new Size(WT.Width, WT.Height);
+            p.TA.Size = new Size(TA.Width, TA.Height);
+
+            //set enable attrib
+            if (pl.Count > 0)
+            {
+                if (pl[0].Prio.ReadOnly == true)
+                    p.Prio.ReadOnly = true;
+                else
+                    p.Prio.ReadOnly = false;
+            }
+            else
+            {
+                p.Prio.ReadOnly = true;
+            }
+            p.WT.ReadOnly = true;
+            p.TA.ReadOnly = true;
+            //add to control
+            Controls.Add(p.name);
+            Controls.Add(p.BT);
+            Controls.Add(p.AT);
+            Controls.Add(p.Prio);
+            Controls.Add(p.WT);
+            Controls.Add(p.TA);
+
+            //add process to list
+            pl.Add(p);
+        }
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            
         }
     }
     public class process
     {
-        TextBox BT, AT, P, WT, TA;
-        Label name;
+        public TextBox BT, AT, Prio, WT, TA;
+        public Label name;
         public int x, y;
 
-        public void show(int i)
+        public process()
         {
             name = new Label();
-            name.Text = "P" + i.ToString() + ":";
-            name.Location = new Point(x, y);
-            name.Show();
             BT = new TextBox();
-            BT.Location = new Point(x, y);
             AT = new TextBox();
-            AT.Location = new Point(x, y);
-            P = new TextBox();
-            P.Location = new Point(x, y);
+            Prio = new TextBox();
             WT = new TextBox();
-            WT.Location = new Point(x, y);
             TA = new TextBox();
-            TA.Location = new Point(x, y);
         }
     }
 }
